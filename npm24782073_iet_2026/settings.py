e8441f01b7c74e48adbcaf2f25432b978ce15f16
Dito1290 <gilangpermadid@gmail.com>
2026-05-20 13:49:50 +0700

    settings10

--- npm24782073_iet_2026/settings.py
+++ npm24782073_iet_2026/settings.py
@@ -133,5 +133,10 @@ MESSAGE_TAGS = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  
     ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',  
+    ],
 }
